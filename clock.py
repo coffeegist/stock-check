62c7b939c2a2e57065a901497cbb02bdc5c27732
Adam Brown <audrummer15@gmail.com>
2017-06-07 21:38:30 -0400

    Pull scheduling interval from environment to allow easier changing of times.

--- clock.py
+++ clock.py
@@ -1,9 +1,10 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+import os
 import stock_check
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=os.environ['CHECK_INTERVAL'])
 def timed_job():
     stock_check.run()
 
