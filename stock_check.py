1526215637b023289ac8da26486c2de32310d48d
Adam Brown <audrummer15@gmail.com>
2017-07-07 09:43:09 -0400

    Read urls from environment variable.

--- stock_check.py
+++ stock_check.py
@@ -10,12 +10,7 @@ LINK_COL = 0 # link
 SMS_COL = 1 # phone number link was sms'ed to
 LINKS_PER_MESSAGE = 10 # send up to 10 links at a time
 
-# urls
-item_urls = [
-    'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=rx+580&N=100007709%20600494828%20600007787&isNodeId=1&LeftPriceRange=0%20400', # 4G/8G RX 580
-    'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&N=100007709%20600007787&IsNodeId=1&Description=rx%20570&name=Desktop%20Graphics%20Cards&Order=BESTMATCH&isdeptsrh=1&LeftPriceRange=0%20400' # 4G RX 570
-]
-
+item_urls = os.environ['ITEM_URLS'].split(',')
 my_numbers = os.environ['MY_NUMBER'].split(',')
 twilio_number = os.environ['TWILIO_NUMBER']
 
